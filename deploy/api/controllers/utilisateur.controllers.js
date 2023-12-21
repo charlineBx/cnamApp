f13d78c178a35eeda86ba923d1974a779e086dbe
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2023-12-21 18:17:47 +0100

    ajout connection user via BDD

--- deploy/api/controllers/utilisateur.controllers.js
+++ deploy/api/controllers/utilisateur.controllers.js
@@ -17,6 +17,38 @@ exports.login = (req, res) => {
   // Test
   let pattern = /^[A-Za-z0-9]{1,20}$/;
   if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
+     Utilisateur.findOne({ where: { login: utilisateur.login } })
+    .then(data => {
+      if (data) {
+        const user = {
+          id: data.id,
+          name: data.nom,
+          email: data.email
+        };
+      
+        let accessToken = generateAccessToken(user);
+        res.setHeader('Authorization', `Bearer ${accessToken}`);
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Utilisateur with login=${utilisateur.login}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(400).send({
+        message: "Error retrieving Utilisateur with login=" + utilisateur.login
+      });
+    });
+  } else {
+    res.status(400).send({
+      message: "Login ou password incorrect" 
+    });
+  }
+};
+/*
+let pattern = /^[A-Za-z0-9]{1,20}$/;
+  if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
     if(utilisateur.login === "emma" && utilisateur.password === "toto"){
 
       const uuid = uuidv4 ();
@@ -43,6 +75,6 @@ exports.login = (req, res) => {
     
       }  
     };    
-};
+*/ 
 
 
