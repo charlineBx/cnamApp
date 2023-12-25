cda9926300baf1e1ad1763e072e1245ef5610ff8
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2023-12-25 16:55:11 +0100

    essai 8

--- deploy/api/controllers/catalogue.controllers.js
+++ deploy/api/controllers/catalogue.controllers.js
@@ -3,6 +3,16 @@ const db = require("../models");
   const Catalogue = db.produit;
   const Op = db.Sequelize.Op;
 
+  const catalogue = [
+	{ titre: "Liv Avail Advanced 1",description:"Vélo de rando/course", prix: 3000, qte: 5, image: "./assets/images/avail-advanced-1.jpg" },
+	{ titre:  "Specialized Dolce Elite", description:"Vélo debutant route",prix: 1550.5, qte: 2 ,image: "./assets/images/specialized-dolce-elite.jpg"},
+	{ titre: "Giant Talon 0",description:"VTT", prix: 950, qte: 1 ,image: "./assets/images/talon-29-0-giant.jpg"},
+	{ titre:  "Liv Amiti 3 E+", description:"Vélo taff",prix: 2400.5, qte: 3,image: "./assets/images/amiti-eplus.jpg" },
+	{ titre: "Canyon Aeroad CF SLX 8.0",description:"Vélo de course", prix: 6000, qte: 2, image: "./assets/images/canyon_cf_sxl.png" },
+	{ titre:  "Canyon Ultimate CF SL 7.0", description:"Vélo de course",prix: 3000, qte: 1 ,image: "./assets/images/canyon_cf_sl_7.jpeg"},
+	{ titre: "Canyon Speedmax CF SL 8.0",description:"Vélo de chrono", prix: 3999, qte: 4 ,image: "./assets/images/canyon_cf_sl_speedmax.jpg"}
+];
+
   exports.get = async (req, res) => {
 	console.log("COUCOU22222");
 	if(req.query.query){
@@ -20,7 +30,7 @@ const db = require("../models");
 				const produits = await Catalogue.findAll({ where: condition });
 				console.log(produits);
 			// Envoi des produits filtrés en tant que réponse
-			res.send(produits);
+			res.send(catalogue);
 
 		//}else {
 		//	res.status(400).send({
@@ -31,7 +41,7 @@ const db = require("../models");
 	}else{
 		console.log("catalogue sans filtre.");
 		const produits = await Catalogue.findAll();
-		res.send(produits);
+		res.send(catalogue);
 	}
    };    
 
